73d7784d8033f3ec96d00a03cc2c59625cc338ef
3Juhwan <64410384+3Juhwan@users.noreply.github.com>
2020-11-10 12:42:58 +0900

    :bookmark: Git test && Amend Code

--- Advanced_C_2020-2/Dynamic-allocation/Easy/11.c
+++ Advanced_C_2020-2/Dynamic-allocation/Easy/11.c
@@ -1,51 +1,44 @@
 #pragma warning (disable:4996)
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
-	int N = 0, cnt = 0, len = 0, i = 0, tcnt = 0;
-	int* p[6] = { NULL };
+	int* p = NULL, q[20] = { 0 };
+	int N = 0, cnt = 0;
 
-	p[cnt] = (int*)malloc(5 * sizeof(int));
-
-	if (p[cnt] == NULL)
+	p = (int*)malloc(5 * sizeof(int));
+	if (p == NULL)
 		return -1;
 
-	while (N != -1 && len < 20)
+	scanf("%d", &p[cnt]);
+	while (p[cnt] != -1 && cnt < 20)
 	{
-	    
-		if (len >= 5 && (len - 5) % 3 == 0)
+		cnt++;
+
+		if (cnt >= 5 && (cnt - 5) % 3 == 0)
 		{
-			cnt++;
-			p[cnt] = (int*)malloc(3 * sizeof(int));
-			if (p[cnt] == NULL)
+			for (int i = 0; i < cnt;i++)
+				q[i] = p[i];
+
+			free(p);
+
+			p = (int*)malloc((cnt + 3) * sizeof(int));
+			if (p == NULL)
 				return -1;
 
-			i = 0;
-		}
-		
-		scanf("%d", &N);
-		p[cnt][i] = N;
-		i++;
-		len++;
-	}
-
-	for (int i = 0; i < 5 && len - tcnt > 0;i++)
-	{
-		printf(" %d", p[0][i]);
-		tcnt++;
-	}
-
-	for (int i = 1; i < cnt + 1;i++)
-		for (int j = 0; j < 3 && len - tcnt > 0;j++)
-		{
-			printf(" %d", p[i][j]);
-			tcnt++;
+			for (int i = 0; i < cnt;i++)
+				p[i] = q[i];
 		}
 
-	for (int i = 0;i < cnt;i++)
-		free(p[i]);
+		scanf("%d", &p[cnt]);
+	}
+	cnt++;
+
+	for (int i = 0; i < cnt;i++)
+		printf(" %d", p[i]);
+
+	free(p);
 
 	return 0;
 }
